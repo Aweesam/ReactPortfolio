ed3ea32525f53013cb9e93da63eae18373120a59
Samuel Adkins <samuel.c.adkins@gmail.com>
2022-06-22 18:13:30 -0400

    Spacing fixed for mobile

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div name="about" className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white">
+    <div name="about" className="w-full lg:h-screen bg-gradient-to-b from-gray-800 to-black text-white sm:h-full">
         <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-            <div className="pb-8">
+            <div className="pb-1 md:pb-8">
                 <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
             </div>
 
@@ -14,8 +14,8 @@ const About = () => {
 
             <br />
 
-            <p className="text-xl">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius delectus, pariatur aliquid dolore esse accusamus nam assumenda suscipit ullam nisi illum, sed harum quis omnis incidunt exercitationem praesentium atque aperiam, vitae quia placeat. Placeat maiores quos, minima aut enim ea non dolore numquam nemo, a vero animi doloremque quidem inventore.
+            <p className="text-xl b">
+                Let's build a better world!
             </p>
         </div>
     </div>
